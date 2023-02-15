6984a12844617cbe02480f0bbb05ac0080d0fc7d
katsuhiro_yamashita <katsuhiro.yamashita.sr01@gmail.com>
2023-02-15 10:39:37 +0900

    2023-02-15

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ io.on("connection", (socket) => {
 
     socket.on('sharingUserInfo', (data) => {
 		//add 2/14
-		currentUsers = currentUsers.filter((item) => item.userId !== data.userId && item.role !== data.role);
+		currentUsers = currentUsers.filter((item) => !(item.userId === data.userId && item.role === data.role));
 		//change 2/4
 		if (!currentUsers.some(
 			b => b.socketId === data.socketId
